a4d80b34c9fa893b5ff90cd770d8b2d7044beb21
hinoneko <yanasiryk35@gmail.com>
2024-12-27 19:57:30 +0200

    Refactor task3

--- task3/task_3.py
+++ task3/task_3.py
@@ -8,20 +8,18 @@ async def long_running_task():
         print("Task finished")
     except asyncio.CancelledError:
         print("Task was cancelled")
+        raise
 
-def demonstrate_cancellation():
-    async def run_demo():
-        task = asyncio.create_task(long_running_task())
-        await asyncio.sleep(1)
-        task.cancel()
-        try:
-            await task
-        except asyncio.CancelledError:
-            print("Handled cancellation")
-
-    return run_demo
 
 async def main():
-    await demonstrate_cancellation()()
+    task = asyncio.create_task(long_running_task())
+
+    await asyncio.sleep(1)
+    task.cancel()
+
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Handled cancellation")
 
 asyncio.run(main())
