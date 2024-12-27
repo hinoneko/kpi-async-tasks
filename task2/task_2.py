57692db99bab7fb365d3f69a42c9a342de97504b
hinoneko <yanasiryk35@gmail.com>
2024-12-27 19:51:05 +0200

    Add task2

---
+++ task2/task_2.py
@@ -0,0 +1,31 @@
+import asyncio
+from random import random
+
+
+async def async_map(lst, func):
+    tasks = [asyncio.create_task(func(item)) for item in lst]
+    results = await asyncio.gather(*tasks)
+
+    return results
+
+
+async def multiply_by_two(x):
+    delay = random()
+    await asyncio.sleep(delay)
+
+    return x * 2
+
+
+async def main():
+    nums_lst = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+
+    cors = [async_map(nums, multiply_by_two) for nums in nums_lst]
+    tasks = asyncio.gather(*cors)
+    results = await tasks
+    print(f"Results: {results}")
+
+asyncio.run(main())
